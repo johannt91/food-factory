a602a04cc97b9fab5a64be30a6c48efee1272830
Johann Taylor <j.taylor1343@gmail.com>
2021-04-16 00:51:15 -0400

    Added offline functionality using service worker

---
+++ service-worker.js
@@ -0,0 +1,64 @@
+const APP_PREFIX = 'FoodFest-';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./events.html",
+    "./tickets.html",
+    "./schedule.html",
+    "./assets/css/style.css",
+    "./assets/css/bootstrap.css",
+    "./assets/css/tickets.css",
+    "./dist/app.bundle.js",
+    "./dist/events.bundle.js",
+    "./dist/tickets.bundle.js",
+    "./dist/schedule.bundle.js"
+];
+
+self.addEventListener('install', function (e) { // self is used instead of window because service workers run before the window object has even been created
+    e.waitUntil( // wait until work is complete before terminating service worker
+        caches.open(CACHE_NAME).then(function (cache) { // find specific cache by name
+            console.log('installing cache : ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE) // add every file in the FILES_TO_CACHE array to the cache
+        })
+    )
+})
+
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            let cacheKeeplist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            })
+            cacheKeeplist.push(CACHE_NAME);
+
+            return Promise.all(
+                keyList.map(function (key, i) {
+                    if (cacheKeeplist.indexOf(key) === -1) {
+                        console.log('deleting cache : ' + keyList[i]);
+                        return caches.delete(keyList[i]);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) {
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            } else {
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch(e.request)
+            }
+            
+            // Alternatively
+            // return request || fetch(e.request)
+        })
+    )
+  })
